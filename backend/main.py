e045dacc1eab513f52bc2da99d38972df350141f
omerfaizan10 <142986580+omerfaizan10@users.noreply.github.com>
2026-07-03 05:50:34 -0400

    added vercel URL in origins

--- backend/main.py
+++ backend/main.py
@@ -96,7 +96,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:3000",
-        "http://127.0.0.1:3000",
+        "http://127.0.0.1:3000","https://transformer-interpretability-studio.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
